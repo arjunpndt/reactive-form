0db32fdbb517fe859d39f95973ec557deb454421
arjunpndt <arjunpndt.edu@gmail.com>
2024-01-09 17:58:31 +0530

    comments added

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -16,16 +16,30 @@ import {
 })
 export class ReactiveFormComponent implements OnInit {
   formHeading = 'Reactive Form';
+  // use credentianl object to show that how two way data binding works
+  credential = { username: '', passowrd: '' };
   reactiveForm: FormGroup;
   ngOnInit() {
+    console.warn(
+      "ngModel with ReactiveForm is deprecated that's why this warning is coming"
+    );
+
     this.reactiveForm = new FormGroup({
       firstname: new FormControl(null, Validators.required),
       lastname: new FormControl(null),
+      username: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, Validators.required),
     });
   }
 
+  //
+
   submitForm() {
-    console.log('Form SUbmitted', this.reactiveForm);
+    console.log('Form Submitted', this.reactiveForm, this.credential);
+    alert('Form Submitted ');
   }
 }
